14bdacf5ab9132ee5be527ae0fd51ae52a941cc9
theclone <mrperson7895@gmail.com>
2018-10-02 14:52:41 -0700

    add wave spawning controller

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject hazard;
+    [SerializeField]
+    private Vector3 spawnValues;
+    [SerializeField]
+    private int hazardCount;
+    [SerializeField]
+    private float spawnGap;
+    [SerializeField]
+    private float startWait;
+    [SerializeField]
+    private float waveWait;
+
+    void Start()
+    {
+        StartCoroutine(SpawnWaves());
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnGap);
+            }
+			yield return new WaitForSeconds(waveWait);
+        }
+    }
+}
