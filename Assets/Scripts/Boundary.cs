2f131878e4f65ebb19ba2ec8aebd860df599687e
theclone <mrperson7895@gmail.com>
2018-10-02 07:44:06 -0700

    add player movement and clamping

---
+++ Assets/Scripts/Boundary.cs
@@ -0,0 +1,7 @@
+[System.Serializable]
+public class Boundary {
+    public float xMin;
+    public float xMax;
+    public float zMin;
+    public float zMax;
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public const string HORIZONTAL = "Horizontal";
+    public const string VERTICAL = "Vertical";
+
+    [SerializeField]
+    private float speed;
+	[SerializeField]
+	private Boundary boundary;
+
+	private Rigidbody rigidbody;
+
+    void Start()
+    {
+		rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+
+        float moveHorizontal = Input.GetAxis(HORIZONTAL);
+        float moveVertical = Input.GetAxis(VERTICAL);
+
+        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+        rigidbody.velocity = movement * speed;
+
+        rigidbody.position = new Vector3
+        (
+            Mathf.Clamp(rigidbody.position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(rigidbody.position.z, boundary.zMin, boundary.zMax)
+        );
+    }
+}
