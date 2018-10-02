3602d778f51a0ce152390d5f2feb74e48a83e626
theclone <mrperson7895@gmail.com>
2018-10-02 11:00:33 -0700

    create bolt prefabs and scripted movement

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+	[SerializeField]
+	private float speed;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody>().velocity = transform.forward * speed;
+	}
+}
