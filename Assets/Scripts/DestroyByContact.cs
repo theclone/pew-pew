86b9515247b4d7e6fd15fae8eae1ef71b8fccc16
theclone <mrperson7895@gmail.com>
2018-10-02 14:39:50 -0700

    add asteroid explosion effects

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour {
 
+	[SerializeField]
+	private GameObject explosion;
+	[SerializeField]
+	private GameObject playerExplosion;
+
 	void OnTriggerEnter(Collider other) 
     {
         if (other.tag == "Boundary")
         {
             return;
         }
+
+		Instantiate(explosion, transform.position, transform.rotation);
+
+		if (other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+        }
+		
         Destroy(other.gameObject);
         Destroy(gameObject);
     }
